534cae6bfca82ddc5ee2edb3f2b55dcce0e0719d
therealrinku <backupfb2032@gmail.com>
2021-03-16 07:08:31 +0545

    game checker function reformatted

--- tictactoe.py
+++ tictactoe.py
@@ -9,7 +9,14 @@ def displayBoard():
 
 
 def gameChecker():
-    if board[0]==board[1]==board[2] or board[3]==board[4]==board[5] or board[6]==board[7]==board[8] or board[0]==board[3]==board[6] or board[1]==board[4]==board[7] or board[2]==board[5]==board[8]or board[0]==board[4]==board[8] or board[2]==board[4]==board[6]:
+    if (board[0]==board[1]==board[2] 
+    or board[3]==board[4]==board[5] 
+    or board[6]==board[7]==board[8] 
+    or board[0]==board[3]==board[6] 
+    or board[1]==board[4]==board[7] 
+    or board[2]==board[5]==board[8]
+    or board[0]==board[4]==board[8] 
+    or board[2]==board[4]==board[6]):
         print("The winner is {}".format(currentPlayer))
         displayBoard();
     else:
